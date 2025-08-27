75aaf180b9a937b5e191a564fe2c166db92bb1ce
anwar907 <rilltheend@gmail.com>
2025-08-27 23:30:21 +0700

    test: unit test auth repository

--- packages/auth_repository/lib/src/auth_repository.dart
+++ packages/auth_repository/lib/src/auth_repository.dart
@@ -1,5 +1,24 @@
 import 'package:supabase_repository/supabase_repository.dart';
 
+class AuthRepositoryException implements Exception {
+  final Exception exception;
+  final StackTrace stackTrace;
+
+  AuthRepositoryException(this.exception, this.stackTrace);
+}
+
+class SigninException extends AuthRepositoryException {
+  SigninException(super.exception, super.stackTrace);
+}
+
+class SignUpException extends AuthRepositoryException {
+  SignUpException(super.exception, super.stackTrace);
+}
+
+class SignOutException extends AuthRepositoryException {
+  SignOutException(super.exception, super.stackTrace);
+}
+
 class AuthRepository {
   final SupabaseApiClient _supabaseRepository;
 
@@ -10,8 +29,8 @@ class AuthRepository {
     try {
       final response = await _supabaseRepository.signIn(email, password);
       return response.session?.accessToken;
-    } catch (e) {
-      throw Exception(e.toString());
+    } on Exception catch (e, stackTrace) {
+      throw SigninException(e, stackTrace);
     }
   }
 
@@ -23,16 +42,16 @@ class AuthRepository {
         username,
       );
       return response.session?.accessToken;
-    } catch (e) {
-      throw Exception(e.toString());
+    } on Exception catch (e, stackTrace) {
+      throw SignUpException(e, stackTrace);
     }
   }
 
   Future<void> signOut() async {
     try {
       await _supabaseRepository.signOut();
-    } catch (e) {
-      throw Exception(e.toString());
+    } on Exception catch (e, stackTrace) {
+      throw SignOutException(e, stackTrace);
     }
   }
 }
--- packages/auth_repository/test/auth_repository_test.dart
+++ packages/auth_repository/test/auth_repository_test.dart
@@ -1,1 +1,77 @@
+import 'package:auth_repository/auth_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_repository/supabase_repository.dart';
 
+class AuthMock extends Mock implements SupabaseApiClient {}
+
+void main() {
+  final supabase = AuthMock();
+  final authRepository = AuthRepository(supabaseRepository: supabase);
+
+  group('Login User', () {
+    test('should throw Exception on failure', () async {
+      when(() => supabase.signIn('', '')).thenThrow(Exception('Failure'));
+
+      await expectLater(
+        authRepository.signIn('', ''),
+        throwsA(isA<SigninException>()),
+      );
+    });
+
+    test('should return access token on success', () async {
+      final mockResponse = AuthResponse(
+        session: Session(
+          accessToken: 'test_token',
+          tokenType: 'bearer',
+          user: User(
+            id: 'user_id',
+            appMetadata: {},
+            userMetadata: {},
+            aud: 'authenticated',
+            createdAt: DateTime.now().toIso8601String(),
+          ),
+        ),
+      );
+
+      when(
+        () => supabase.signIn('test@email.com', 'password'),
+      ).thenAnswer((_) async => mockResponse);
+
+      final result = await authRepository.signIn('test@email.com', 'password');
+      expect(result, 'test_token');
+    });
+  });
+
+  group('Sign Up User', () {
+    test('should throw Exception on failure', () async {
+      when(() => supabase.signUp('', '', '')).thenThrow(Exception('Failure'));
+
+      await expectLater(
+        authRepository.signUp('', '', ''),
+        throwsA(isA<SignUpException>()),
+      );
+    });
+
+    test('should return access token on success', () async {
+      when(() => supabase.signUp('name', 'email', 'password')).thenAnswer(
+        (_) async => AuthResponse(
+          session: Session(
+            tokenType: 'bearer',
+            user: User(
+              id: 'user_id',
+              appMetadata: {},
+              userMetadata: {},
+              aud: '',
+              createdAt: '',
+            ),
+            accessToken: 'token',
+          ),
+        ),
+      );
+      final result = await authRepository.signUp('name', 'email', 'password');
+      expect(result, 'token');
+    });
+  });
+}
